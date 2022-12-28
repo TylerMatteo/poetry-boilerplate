220be3a9a7a2495be12d48d41fa8b9d7bdcd6955
Tyler Matteo <tylermatteo434@gmail.com>
2022-12-28 15:47:13 -0500

    Install pylint and black, generate pylintrc file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+"""A simply script that prints hello world!"""
+
+
 def hello_world():
-  print("Hello world!")
+    "A callable function that prints hello world!"
+    print("Hello world!")
+
 
 if __name__ == "__main__":
-  hello_world()
+    hello_world()
